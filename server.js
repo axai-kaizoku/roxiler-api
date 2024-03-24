d340de5366e8a9dbe01f4de5e5bce97aecd1eaba
axai <02b3akshay@gmail.com>
2024-03-24 16:50:43 +0530

    Add files

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const transactionRoute = require('./routes/transactionRoute');
+const connectDB = require('./database/index');
+const seedData = require('./config/seed-data');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.SERVER_PORT || 8080;
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api/v1/transaction', transactionRoute);
+
+app.get('/', (req, res) => {
+	return res.json({ message: 'Server is up and running!' });
+});
+
+Promise.all([connectDB(), seedData()])
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is running on http://localhost:${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error('Failed to initialize server:', error);
+	});
